60f58dc4029244eb9db2c04cccb3e6a39769c8a4
shreya <shreyavats2407@gmail.com>
2025-02-27 12:10:10 +0530

    comment added

--- src/customs/customHooks.jsx
+++ src/customs/customHooks.jsx
@@ -2,3 +2,4 @@ import {useSelector,useDispatch} from "react-redux"
 
 export const useAppSelector = useSelector;
 export const useAppDispatch = ()=> useDispatch();
+// u can keep them in store only no issue 
